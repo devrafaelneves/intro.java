76307fc88fe2eca4ee397f57d72ab20ee5041a64
ghstN <98927555+ghstN@users.noreply.github.com>
2022-09-19 23:07:50 -0300

    Add files via upload

---
+++ intro_java/HelloWorld.java
@@ -0,0 +1,160 @@
+//nove
+public class HelloWorld{
+    public static void main (String[]args){
+
+        double valor = 10;
+        valor += 10;
+
+        System.out.println(valor); //20
+
+        valor -= 15;
+        System.out.println(valor); //5
+
+        valor *= 2;
+        System.out.println(valor); //10
+
+        valor /= 10;
+        System.out.println(valor); //1
+
+
+}
+}
+
+
+//oito 
+// public class HelloWorld{
+//     public static void main (String[]args){
+        
+//         float n1 = 8;
+//         float n2 = 9;
+       
+
+//         boolean maior = (n1 > n2);
+//         boolean menor = (n1 < n2);
+//         boolean igual = (n1 == n2);
+//         boolean diferente = (n1 != n2);
+
+
+//         System.out.println(maior);
+//         System.out.println(menor);
+//         System.out.println(igual);
+//         System.out.println(diferente);
+
+// }
+// }
+
+
+
+//sétimo
+// public class HelloWorld{
+//     public static void main (String[]args){
+        
+//         float n1 = 8;
+//         float n2 = 9;
+//         float n3 = 10;
+//         float res = (n1 + n2 + n3)/3 ;
+//         float exame = 10-res;
+
+//         if (res >= 7 ){
+//             System.out.println("Você está aprovado");
+//         }
+//         else {
+//             System.out.println("Você está reprovado. Você precisa tirar " + (exame) + " no exame para recuperar a média.");
+//         }
+
+//     }
+
+// }
+
+//sexto
+// public class HelloWorld{
+//     public static void main (String[]args){
+        
+//         float n1 = 2;
+//         float n2 = 3;
+//         float n3 = 4;
+//         float res = (n1 + n2 + n3)/3 ;
+//         float exame = 10-res;
+
+//         if (res >= 7 ){
+//             System.out.println("Você está aprovado");
+//         }
+//         else {
+//             System.out.println("Você está reprovado. Você precisa tirar " + (exame) + " no exame para recuperar a média.");
+//         }
+
+//     }
+
+// }
+
+//quinto 
+// public class HelloWorld{
+//     public static void main (String[]args){
+        
+//         float n1 = 6;
+//         float n2 = 7;
+//         float n3 = 8;
+//         float res = (n1 + n2 + n3)/3 ;
+
+//         System.out.println(res);
+//     }
+
+// }
+
+
+//quarto
+// public class HelloWorld{
+//     public static void main (String[]args){
+        
+//         String nome = " Rafael";
+//         int idade =  20;
+//         int a =  1;
+//         int b =  2;
+
+//         System.out.println(a + b + nome );
+//         System.out.println(a - b);
+//         System.out.println(a*b);
+//         System.out.println(a/b);
+//     }
+
+// }
+
+
+
+//Terceiro
+// public class HelloWorld{
+//     public static void main (String[]args){
+        
+//         String nome = "Rafael";
+//         int idade = 20;
+        
+    
+//         System.out.println(idade);
+//     }
+
+// }
+
+
+//segundo
+// public class HelloWorld{
+//     public static void main (String[]args){
+        
+//         String nome = "Rafael";
+        
+    
+//         System.out.println(nome);
+//     }
+
+// }
+
+//Primeiro
+//Foi criado uma class e depois só chamou no terminal
+// PS C:\Users\aluno\Desktop\intro_java> javac HelloWorld.java
+// PS C:\Users\aluno\Desktop\intro_java> java HelloWorld
+// Hello World
+// PS C:\Users\aluno\Desktop\intro_java>
+
+//A ORDEM É SEMPRE USAR O TERMINAL COM OS CODE :
+//javac (nome do arquivo.java)
+//depois ->
+//java (nome do arquivo só)
